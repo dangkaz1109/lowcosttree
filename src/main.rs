dc8ae33e5f291b7eba613e74a2ccd362ed55d3dd
Fiano <neurocurior1109@gmail.com>
2025-06-29 14:30:38 +0700

    hello

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+
+
+use std::ptr
+use std::alloc::{alloc, dealloc}
+
+
+struct Node {
+    data: f32,
+    height: usize,
+    left: Option<*mut Node>,
+    right: Option<*mut Node>,
+}
+impl Node {
+    fn new(data: f32) -> Node {
+        Node {
+            data: data,
+            height: 0,
+            left: None,
+            right: None
+        }
+    }
+}
+
+struct Tree {
+    root: Node,
+}
+impl Tree {
+    fn new(root: Node) ->Self {
+        Self {
+            root: root,
+        }
+    }
+    fn getHeight(root: &Node) -> usize {
+        if root
+        root.height
+    }
+    fn getBalance(root: &Node) -> isize {
+        
+    }
+    
+}
